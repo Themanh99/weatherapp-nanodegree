9052cb072d56b0d4b62d1aab53e59e8cd1d71ab9
Themanh99 <themanhchu99@gmail.com>
2023-09-05 02:35:12 +0700

    add code logic done

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ require("dotenv").config();
  * @data is object data responsed by server
  */
 const port = process.env.PORT || 5000;
-const apiKey = `${process.env.API_KEY}&units=imperial`;
-const data = {};
+let projectData = {};
 /**
  * Define some library that will be used
  */
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 /**
  * Define app by express middleware
@@ -29,11 +29,17 @@ app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
 
+app.use(express.static(path.join(__dirname, "website")));
 /**
  * Get method
  */
 app.get("/data", (req, res) => {
-  res.status(200).send(data);
+  try {
+    console.log("GET", projectData);
+    res.status(200).send(projectData);
+  } catch (error) {
+    next(error);
+  }
 });
 
 /**
@@ -43,9 +49,17 @@ app.post("/data", sendData);
 
 function sendData(req, res, next) {
   try {
+    projectData = {
+      name: req.body.name,
+      date: req.body.date,
+      temp: req.body.temp,
+      content: req.body.content,
+    };
+    console.log("POST", projectData);
     res.status(200).send({
-      data: data,
-      message: "Successfully!",
+      data: projectData,
+      message: "Send Data Successfully!",
+      succes: true,
     });
   } catch (error) {
     next(error);
--- website/app.js
+++ website/app.js
@@ -1,1 +1,95 @@
-const url = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=c96e9d8d3c7b7e4a1346577c0bddbdad`;
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey = "7ca78e271675476a6a167286fe79f3b2&units=imperial";
+const urlServer = "http://localhost:3077/data";
+
+/**
+ * Get weather data from Openweather with url params
+ * @params @url
+ * @return @data
+ */
+const getApi = async (url) => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const postApi = async (url, data) => {
+  try {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const handleGetData = async () => {
+  const feel = document.getElementById("emotion");
+  const zipcode = document.getElementById("zipcode");
+
+  if (zipcode.value.length == 0 || feel.value.length == 0) {
+    window.alert("You need fill all fields, example: zipcode = 94040");
+    return;
+  }
+  const url = baseUrl + zipcode.value + `&appid=${apiKey}`;
+  let result = await getApi(url);
+
+  if (result.message) {
+    alert(result.cod + ": " + result.message, "Input zipcode again");
+    return;
+  }
+  // Create a new Date object and set it using the result.dt (in milliseconds)
+  const date = new Date(result.dt * 1000);
+
+  const day = String(date.getDate()).padStart(2, "0"); // Add leading zero if needed
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Add 1 to month (zero-based index)
+  const year = date.getFullYear();
+
+  // Format the date as DD-MM-YYYY
+  const formattedDate = `${day}-${month}-${year}`;
+
+  const data = {
+    name: result.name,
+    temp: result.main.temp,
+    content: feel.value,
+    date: formattedDate,
+  };
+
+  //Send data to server
+  await postApi(urlServer, data);
+
+  /**
+   * Get data from server then display it on the page
+   */
+  updateScreen();
+};
+
+const updateScreen = async () => {
+  //Get data from the server
+  let dataJson = await getApi(urlServer);
+
+  try {
+    // const dataJson = await data.json();
+    document.getElementById("name").innerHTML = dataJson.name;
+    document.getElementById("date").innerHTML = `Date: ` + dataJson.date;
+    document.getElementById("temp").innerHTML =
+      `Temp: ` + Math.round(dataJson.temp) + "degrees";
+    document.getElementById("content").innerHTML =
+      `Content: ` + dataJson.content;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const btn = document.querySelector("#generate");
+btn.addEventListener("click", handleGetData);
